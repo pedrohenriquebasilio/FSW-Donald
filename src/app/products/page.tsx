86205ddc3d8667f2036a69f13b79d11e02f9db06
Pedro Basilio <phbasilio6903@gmail.com>
2025-02-28 20:53:28 -0300

    feat: add shacdn, inital setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1 className="p-5">Products principal</h1>
+      <Input placeholder="bora de projetinho pae?" />
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default ProductsPage;
